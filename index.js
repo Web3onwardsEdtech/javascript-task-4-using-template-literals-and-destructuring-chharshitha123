8f1b03e9d73b1f833f550386d6dccfdc0993555a
Harshitha <chharshitha277@gmail.com>
2024-10-05 16:05:57 +0530

    "task 4 js backend completed"

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+const username=process.argv[2];
+const userage=parseInt(process.argv[3]);
+const usercountry=process.argv[4];
+function greetingmsg(name,age,country){
+    let msg=`hi! my name is ${name}, i am ${age} years old and i am from ${country}`;
+    console.log(msg);
+}
+greetingmsg(username,userage,usercountry);
